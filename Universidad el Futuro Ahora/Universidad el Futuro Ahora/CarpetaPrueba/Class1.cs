c008dd01500578bb7b27b41103f41d1c6fd81c76
JASON JESUS GONZALEZ SALAS <jason.gonzalez1@uamcr.net>
2022-02-10 21:34:12 -0600

    PruebaCommit
    Creacion de carpeta prueba y clase

---
+++ Universidad el Futuro Ahora/Universidad el Futuro Ahora/CarpetaPrueba/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Universidad_el_Futuro_Ahora.CarpetaPrueba
+{
+    public class Class1
+    {
+    }
+}
