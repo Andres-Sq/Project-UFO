440757d077f769a32bbde1e46edea8ef4bba1272
Andres <66532891+Andres-Sq@users.noreply.github.com>
2022-02-19 19:53:34 -0600

    Datos en tablas
    Datos provisionales

--- DB-ProjectUFO/DB-ProjectUFO/SQLQuery1.sql
+++ DB-ProjectUFO/DB-ProjectUFO/SQLQuery1.sql
@@ -1,25 +1,25 @@
 USE ProyectoUFO_DB
 GO
 
-INSERT INTO Carrera(CarrerarID, Nombre) VALUES ()
+INSERT INTO Carrera(CarrerarID, Nombre) VALUES ('CB-0108', 'Contaduria')
 
-INSERT INTO Curso(CursoID, Nombre, CarreraID) VALUES ()
+INSERT INTO Curso(CursoID, Nombre, CarreraID) VALUES (1001,'CE-1807','Calculo I')
 
-INSERT INTO CursosPorEstudiantes(EstudianteID, CursoID, Nota) VALUES ()
+INSERT INTO CursosPorEstudiantes(EstudianteID, CursoID, Nota) VALUES (301570178, 1001, 85)
 
 INSERT INTO Estados(EstadoID, Nombre) VALUES (1, 'Abierto')
 INSERT INTO Estados(EstadoID, Nombre) VALUES (2, 'Finalizado')
 INSERT INTO Estados(EstadoID, Nombre) VALUES (3, 'Cerrado')
 
-INSERT INTO Estudiante(EstudianteID, Nombre, Apellidos, Carnet) VALUES ()
+INSERT INTO Estudiante(EstudianteID, Nombre, Apellidos, Carnet) VALUES (301570178, 'Manuel', 'Guevara', 'Arias', 202210010977)
 
-INSERT INTO Matricula(MatriculaID, Estado) VALUES ()
+INSERT INTO Matricula(MatriculaID, Estado) VALUES (10002, 'Abierto')
 
-INSERT INTO MatriculaPorEstudiante(MatriculaID, EstudianteID) VALUES()
+INSERT INTO MatriculaPorEstudiante(MatriculaID, EstudianteID) VALUES(2001, 301570178)
 
-INSERT INTO Notas(NotaID, NotaAprobacion, FechaUltimoCambio) VALUES ()
+INSERT INTO Notas(NotaID, NotaAprobacion, FechaUltimoCambio) VALUES (3001, 85, 01/15/2019)
 
-INSERT INTO PeriodoLectivo(PeriodoID, Nombre, FechaInicio, FechaFinal, Estado, MatriculaID) VALUES ()
+INSERT INTO PeriodoLectivo(PeriodoID, Nombre, FechaInicio, FechaFinal, Estado, MatriculaID) VALUES ('A001','Manuel',01/22/2020,04/13/2020,'Abierto',2001)
 
 
 
