32897224dee8554d8300a8110da0f5fe4b778e8e
Andres <66532891+Andres-Sq@users.noreply.github.com>
2022-02-19 19:25:18 -0600

    Pequeños cambios de orden
    Orden de tablas

--- DB-ProjectUFO/DB-ProjectUFO/SQLQuery1.sql
+++ DB-ProjectUFO/DB-ProjectUFO/SQLQuery1.sql
@@ -1,22 +1,36 @@
 USE ProyectoUFO_DB
 GO
 
-INSERT INTO Estados(EstadoID, Nombre) VALUES (1, 'Abierto')
-INSERT INTO Estados(EstadoID, Nombre) VALUES (2, 'Finalizado')
-INSERT INTO Estados(EstadoID, Nombre) VALUES (3, 'Cerrado')
-
-INSERT INTO PeriodoLectivo(PeriodoID, Nombre, FechaInicio, FechaFinal, Estado, MatriculaID) VALUES ()
-
-INSERT INTO Matricula(MatriculaID, Estado) VALUES ()
-
-INSERT INTO Estudiante(EstudianteID, Nombre, Apellidos, Carnet) VALUES ()
-
-INSERT INTO MatriculaPorEstudiante(MatriculaID, EstudianteID) VALUES()
-
 INSERT INTO Carrera(CarrerarID, Nombre) VALUES ()
 
 INSERT INTO Curso(CursoID, Nombre, CarreraID) VALUES ()
 
 INSERT INTO CursosPorEstudiantes(EstudianteID, CursoID, Nota) VALUES ()
 
+INSERT INTO Estados(EstadoID, Nombre) VALUES (1, 'Abierto')
+INSERT INTO Estados(EstadoID, Nombre) VALUES (2, 'Finalizado')
+INSERT INTO Estados(EstadoID, Nombre) VALUES (3, 'Cerrado')
+
+INSERT INTO Estudiante(EstudianteID, Nombre, Apellidos, Carnet) VALUES ()
+
+INSERT INTO Matricula(MatriculaID, Estado) VALUES ()
+
+INSERT INTO MatriculaPorEstudiante(MatriculaID, EstudianteID) VALUES()
+
 INSERT INTO Notas(NotaID, NotaAprobacion, FechaUltimoCambio) VALUES ()
+
+INSERT INTO PeriodoLectivo(PeriodoID, Nombre, FechaInicio, FechaFinal, Estado, MatriculaID) VALUES ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
